3390ae93af3bc9433d92a6b6d98c8aa74afc10d0
lior margalit <liormarga@gmail.com>
2021-10-29 12:44:12 +0300

    Add project files.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,44 @@
+﻿using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            //throw new InvalidOperationException("Fuck it Error");            
+            return View();
+        }
+
+        [HttpGet("reserve")]
+        public IActionResult Reserve()
+        {
+            //throw new InvalidOperationException("Fuck it Error");            
+            return View();
+        }
+
+        [HttpPost("reserve")]
+        public IActionResult Reserve(ReserveViewModel model)
+        {
+            //throw new InvalidOperationException("Fuck it Error");            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About us";
+            return View();
+        }
+    }
+}
---
+++ ViewModels/ReserveViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DutchTreat.ViewModels
+{
+  public class ReserveViewModel
+  {
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Restaurant { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Date { get; set; }
+
+        [Required]
+        public string Time { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+    }
+}
