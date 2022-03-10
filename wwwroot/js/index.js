f12dc62a073cd1c3e29909d40a371da65b04dcee
lior margalit <liormarga@gmail.com>
2022-03-10 21:15:47 +0200

    version 17

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -631,7 +631,7 @@ setTimeout(async () => {
             //el("speak").style.display = "inline-block"
             el("send").style.display = "inline-block"
             el("message").disabled = false
-            el("message").placeholder = "Type your message 16"
+            el("message").placeholder = "Type your message 17"
             el("interim").innerText = ""
             el("microphone").src = "../images/microphone.png"
             if (el("message").value == "" && el("history").childElementCount > 0 && !el("history").lastChild.innerHTML.includes("please wait") && recognizing) {                
