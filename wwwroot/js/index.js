20f9d9c38452ad0ca1ac330ee7a4ad6249c610f8
lior margalit <liormarga@gmail.com>
2022-03-10 23:37:32 +0200

    add list for ios

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -610,6 +610,12 @@ setTimeout(async () => {
         recognition.interimResults = true
         recognition.lang = "en-US"
         recognition.maxAlternatives = 1;
+        if (iOS()) {
+            var grammar = '#JSGF V1.0; grammar numbers; public <number> = אפס | אחד |שתיים | שלוש | ארבע | חמש | שש| שבע | שמונה | תשע ;'
+            var speechRecognitionList = new SpeechGrammarList();
+            speechRecognitionList.addFromString(grammar, 1);
+            recognition.grammars = speechRecognitionList;
+        }
 
         // switch to listening mode
         recognition.onstart = function () {
